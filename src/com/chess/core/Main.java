dab13f6e8456ac433b21e37ec93e9972938434ea
helioproduct <helioproduct.sync@gmail.com>
2020-11-29 18:07:45 +0500

    Game class update

--- src/com/chess/core/Main.java
+++ src/com/chess/core/Main.java
@@ -3,18 +3,13 @@ package com.chess.core;
 import com.chess.core.GUI.MainFrame;
 import com.chess.core.board.Board;
 import com.chess.core.game.Alliance;
-import com.chess.core.game.Game;
-import com.chess.core.game.player.Player;
 import com.chess.core.pieces.Piece;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-
 
 public class Main {
     public static void main(String[] args) {
-
         Board board = new Board();
+        System.out.println(board);
 
         MainFrame mainFrame = new MainFrame();
         mainFrame.init();
@@ -23,6 +18,5 @@ public class Main {
         mainFrame.drawPieces((ArrayList<Piece>) board.getPieces(Alliance.WHITE));
         mainFrame.showCheckPopup();
         mainFrame.showCheckMatePopup();
-
     }
 }
