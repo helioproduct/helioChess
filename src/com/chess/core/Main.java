c47f46811bfadaf59abe5006b6312aad770ffdc5
helioproduct <helioproduct.sync@gmail.com>
2020-12-03 18:23:52 +0500

    King bug fixed

--- src/com/chess/core/Main.java
+++ src/com/chess/core/Main.java
@@ -3,18 +3,11 @@ package com.chess.core;
 import com.chess.core.GUI.MainFrame;
 import com.chess.core.board.Board;
 import com.chess.core.game.Alliance;
-import com.chess.core.pieces.Queen;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Board board = new Board();
-        board.clearBoard();
-
-        board.setPiece(52, new Queen(board, 52, Alliance.WHITE));
-        board.updateLegalMoves();
+        board.visualizeTileAlliance();
 
         MainFrame mainFrame = new MainFrame();
         mainFrame.init();
@@ -24,11 +17,6 @@ public class Main {
         mainFrame.showCheckPopup();
         mainFrame.showCheckMatePopup();
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            int position = scanner.nextInt();
-            if (position == - 1) break;
-            board.visualizeLegalMoves(position);
-        }
+        for (int i = 48; i < 64; i++) board.visualizeLegalMoves(i);
     }
 }
