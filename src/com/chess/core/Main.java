5a3d4a7a19ca1c5c6aec16c73b7e7d32a9397632
helioproduct <helioproduct.sync@gmail.com>
2020-12-04 21:17:28 +0500

    pieces new icons

--- src/com/chess/core/Main.java
+++ src/com/chess/core/Main.java
@@ -5,20 +5,20 @@ import com.chess.core.board.Board;
 import com.chess.core.game.Alliance;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
-        board.visualizeTileAlliance();
 
         MainFrame mainFrame = new MainFrame();
         mainFrame.init();
         mainFrame.drawBoard(board);
         mainFrame.drawPieces(board.getPieces(Alliance.BLACK));
         mainFrame.drawPieces(board.getPieces(Alliance.WHITE));
-        mainFrame.showLegalMoves(board.getPiece(62).getLegalMovesPositions());
 
-        System.out.println(Arrays.toString(board.getPiece(48).getLegalMovesPositions()));
-
+        Scanner scan = new Scanner(System.in);
+        int position = scan.nextInt();
+        mainFrame.showLegalMoves(board.getPiece(position).getLegalMovesPositions());
     }
 }
