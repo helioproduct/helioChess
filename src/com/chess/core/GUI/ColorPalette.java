e125d4e54cd1684f5342c1aa806b6c97c0bed7ea
helioproduct <helioproduct.sync@gmail.com>
2020-12-05 13:32:49 +0500

    Color palette update

--- src/com/chess/core/GUI/ColorPalette.java
+++ src/com/chess/core/GUI/ColorPalette.java
@@ -6,6 +6,8 @@ public class ColorPalette {
 
     public static final Color TILE_DARK = new Color(209, 138, 70);
     public static final Color TILE_LIGHT = new Color(254, 206, 158);
+    public static final Color BACKGROUND = new Color(205, 147, 90);
+    public static final Color BOARD_STROKE = new Color(125, 83, 42);
 
     // Black and White get from Color-class
 }
